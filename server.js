a62a8cb6a161039c04ae3c85aa4fe1655ba42207
U-DESKTOP-8DA70HV\Possibillion <kalyanamsushma000@gmail.com>
2021-05-17 12:17:29 +0530

    changes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use("/admin", Admin);
 app.use("/resource", Resource);
 app.use("/city", City);
 app.use("/country", Country);
+app.use("/superadminauth", require("./routes/superadmin.js"));
 const CONNECTION_URL =
   "mongodb+srv://kalyani:sushma1997@cluster0.9rilk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5050;
